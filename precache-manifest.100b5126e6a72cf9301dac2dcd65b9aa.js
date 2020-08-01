cefefac2075458b828c545b10705f2504ce39e16
Zorba11 <zorbathegreek111@gmail.com>
2020-08-01 09:33:25 -0400

    Updates

---
+++ precache-manifest.100b5126e6a72cf9301dac2dcd65b9aa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a1f1bd63853a453041da1cf3f3ec12",
+    "url": "/react-deploy-trial/index.html"
+  },
+  {
+    "revision": "af5ec0d32f7a20e4ef54",
+    "url": "/react-deploy-trial/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2a75dddfbf14e337e070",
+    "url": "/react-deploy-trial/static/js/2.677abdd8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy-trial/static/js/2.677abdd8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af5ec0d32f7a20e4ef54",
+    "url": "/react-deploy-trial/static/js/main.bf77ebf7.chunk.js"
+  },
+  {
+    "revision": "211835169cbc77c31333",
+    "url": "/react-deploy-trial/static/js/runtime-main.2f5e755a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy-trial/static/media/logo.5d5d9eef.svg"
+  }
+]);
